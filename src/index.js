5e110bfe6222566e454b3da018277bbc80f84646
Seyi <oluseyi.ogunbanjo@gmail.com>
2020-12-27 19:38:05 -0500

    Asset management: CSS+Images+Bgnd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import _ from 'lodash';
+import './style.css';
+import HeliIcon from './AW109SP.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
+
 function component() {
     const element = document.createElement('div');
   
-    // Lodash, currently included via a script, is required for this line to work
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
   
+    // Add the image to our existing div.
+    const myHeliIcon = new Image();
+    myHeliIcon.src = HeliIcon;
+
+    element.appendChild(myHeliIcon);
+
+    console.log(Data);
+    console.log(Notes);
+    
     return element;
   }
   
